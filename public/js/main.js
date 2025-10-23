8308e0129e6432c0e339dc2f877c3b536e90fb93
Julian Hartwig <Julianha@web.de>
2025-10-23 12:08:25 +0200

    Ordnerstruktur überarbeitet und aufgeteilt für bessere Übersicht. Code hierfür getrennt

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// js/main.js
+// Dieser Code wird ausgeführt, sobald die Seite geladen ist.
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    console.log("Projectify Pro Seite geladen.");
+
+    // Hier können Sie zukünftige Interaktionen hinzufügen,
+    // z.B. für Formularvalidierung oder Smooth-Scrolling.
+
+});
